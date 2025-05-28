1e6ae7a26a8d64f12892c562ddbb0fe0b36e84c0
Daksh Thakran <daksh.thakran05@gmail.com>
2025-05-28 12:47:39 +0530

    Update auth.route.js

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -7,7 +7,7 @@ const router = express.Router();
 
 router.post("/signup", signup);
 router.post("/login", login);
-router.post("/logout", protectRoute, logout);
+router.post("/logout", logout);
 
 router.get("/users/fetch/mentors", protectRoute, getMentors);
 
